9752ae581335b8fcbaa9a525e3a665bb22f2bac3
Vegard Schøyen Houg <vegardhoug246@gmail.com>
2026-03-13 11:38:41 +0100

    Implement powerline customization with color gradient

--- segments/lan_ip.sh
+++ segments/lan_ip.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=bash
 # Prints the local network IPv4 address for a statically defined NIC or search for an IPv4 address on all active NICs.
-TMUX_POWERLINE_SEG_LAN_IP_SYMBOL="${TMUX_POWERLINE_SEG_LAN_IP_SYMBOL:-ⓛ }"
+TMUX_POWERLINE_SEG_LAN_IP_SYMBOL="${TMUX_POWERLINE_SEG_LAN_IP_SYMBOL:-󰌗 }"
 TMUX_POWERLINE_SEG_LAN_IP_SYMBOL_COLOUR="${TMUX_POWERLINE_SEG_LAN_IP_SYMBOL_COLOUR:-255}"
 
 generate_segmentrc() {
